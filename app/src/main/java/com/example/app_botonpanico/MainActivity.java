52944a2e88fca9abb254d2ab0faba6790946adb5
Ivan V <ivanntonio@gmail.com>
2024-03-18 15:20:06 -0600

    feature-Se agregaron pantallas nuevas

--- app/src/main/java/com/example/app_botonpanico/MainActivity.java
+++ app/src/main/java/com/example/app_botonpanico/MainActivity.java
@@ -12,21 +12,31 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button singInButton;
+    Button singInButton,logInButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         singInButton=findViewById(R.id.sing_in_button_activitymain);
+        logInButton=findViewById(R.id.log_in_button_activitymain);
 
         singInButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MainActivity.this,sing_in_user.class);
+                Intent i = new Intent(MainActivity.this, sign_in_user.class);
                 startActivity(i);
             }
         });
+
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, data_register.class);
+                startActivity(i);
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
