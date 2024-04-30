091f3a3728edbed2f68eb0435764532e151e4425
IvanV <ivanntonio@gmail.com>
2024-04-29 23:47:16 -0600

    feature-estilos

--- app/src/main/java/com/example/app_botonpanico/qa_main_menu.java
+++ app/src/main/java/com/example/app_botonpanico/qa_main_menu.java
@@ -6,6 +6,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,16 +16,17 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class qa_main_menu extends AppCompatActivity {
 
-    Button ToMapButton, ToContactsButtom, ToPanicButtom, LogOutButtom;
+    Button ToMapButton, ToContactsButtom, ToPanicButtom;
+    ImageButton LogOutButtom;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_qa_main_menu);
         ToMapButton=findViewById(R.id.ToMap_button_activityqa_MainMenu);
-        ToContactsButtom=findViewById(R.id.ToContacts_button_activityqa_MainMenu);
-        ToPanicButtom=findViewById(R.id.ToPanicButtom_button_activityqa_MainMenu);
-        LogOutButtom=findViewById(R.id.LogOut_button_activityqa_MainMenu);
+        ToContactsButtom=findViewById(R.id.ToContacts_button_activityQaMainMenu);
+        ToPanicButtom=findViewById(R.id.ToPanicButtom_button_activityQaMainMenu);
+        LogOutButtom=findViewById(R.id.imageButton_LogOut_ActivityQaMainMenu);
         ToMapButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
